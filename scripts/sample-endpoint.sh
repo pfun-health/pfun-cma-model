406bd3339b7199f03c8deaab8d18e714f1068fa6
Robbie Capps <rocapp@gmail.com>
2023-08-19 10:02:34 -0700

    hm idk got real close though

--- scripts/sample-endpoint.sh
+++ scripts/sample-endpoint.sh
@@ -2,7 +2,9 @@
 source ${HOME}/Git/pfun-cma-model/.envrc
 proxy_secret=$(aws secretsmanager get-secret-value --secret-id pfun-cma-model-rapid-api-proxy-secret --region us-east-1 | jq -r '.SecretString')
 api_key=$(aws secretsmanager get-secret-value --secret-id pfun-cma-model-rapidapi-key --region us-east-1 | jq -r '.SecretString')
-curl --request POST --url http://"$(chalice url)" \
+endpoint=${1:-run-at-time}
+base_url=$(chalice url)
+curl --request POST --url ${base_url}/${endpoint} \
 	--header "X-RapidAPI-Proxy-Secret: $proxy_secret" \
 	--header "X-RapidAPI-Key: $api_key" \
 	--header "Content-Type: application/json" \
