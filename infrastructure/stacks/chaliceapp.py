525eccf267dc80cac2d618e5f84a41cc65d6e96c
Robbie Capps <rocapp@gmail.com>
2023-08-23 17:39:27 -0700

    mussing et all ep

---
+++ infrastructure/stacks/chaliceapp.py
@@ -0,0 +1,46 @@
+import os
+
+try:
+    from aws_cdk import core as cdk
+except ImportError:
+    import aws_cdk as cdk
+from aws_cdk import aws_autoscaling as autoscaling
+
+from chalice.cdk import Chalice
+
+
+RUNTIME_SOURCE_DIR = os.path.join(
+    os.path.dirname(os.path.dirname(__file__)), os.pardir, 'runtime')
+
+
+class ChaliceApp(cdk.Stack):
+
+    def __init__(self, scope, id, **kwargs):
+        super().__init__(scope, id, **kwargs)
+        self.chalice = Chalice(
+            self, 'PFunCMAModelChaliceApp', source_dir=RUNTIME_SOURCE_DIR,
+            stage_config={
+                'environment_variables': {
+                }
+            }
+        )
+
+        self.chalice.source_repository = 'https://github.com/rocapp/pfun-cma-model'
+        self.chalice.stage_config['name'] = 'dev'
+        self.chalice.stage_config['lambda_memory_size'] = 256
+        self.chalice.stage_config['lambda_timeout'] = 15
+
+        launch_configuration = autoscaling.CfnLaunchConfiguration(
+            self, "PFunCMAModelLaunchConfiguration",
+            image_id='YOUR_AMI_ID',
+            instance_type='YOUR_INSTANCE_TYPE',
+            # other configuration options
+        )
+
+        autoscaling_group = autoscaling.CfnAutoScalingGroup(
+            self, "PFunCMAModelScalingGroup",
+            min_size='1',
+            max_size='10',
+            desired_capacity='5',
+            launch_configuration_name=launch_configuration.ref,
+        )
