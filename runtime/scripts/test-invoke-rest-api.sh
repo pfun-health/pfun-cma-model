f8b5462f616866988b7ef1368a87b9ba7d9e6439
Robbie Capps <rocapp@gmail.com>
2023-09-09 15:21:33 -0700

    probably need to set x-api-key explicitly

--- runtime/scripts/test-invoke-rest-api.sh
+++ runtime/scripts/test-invoke-rest-api.sh
@@ -3,7 +3,13 @@
 source ./scripts/get-auth-headers.sh
 source ./scripts/get-api-endpoint-url.sh
 
-aws apigateway test-invoke-method --http-method POST \
+authorizer_id=v0opmq
+aws apigateway test-invoke-authorizer \
+    --rest-api-id "${api_id}" \
+    --authorizer-id "${authorizer_id}" \
+    --headers "${AUTH_HEADERS}"
+
+aws apigateway test-invoke-method --http-method GET \
     --headers "${AUTH_HEADERS}" \
     --rest-api-id "${api_id}" \
     --resource-id "${run_resource_id}"
