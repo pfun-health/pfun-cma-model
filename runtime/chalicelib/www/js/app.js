ed0d04a7759e4e9e9308ada92c8478e450acb415
Robbie Capps <rocapp@gmail.com>
2023-09-11 00:48:51 -0700

    successful deployment at last. even the custom domain is working... currently trying to get the chart to update

--- runtime/chalicelib/www/js/app.js
+++ runtime/chalicelib/www/js/app.js
@@ -2,6 +2,50 @@
 var apigClient = null;
 var app = {};
 
+// Create an HTML form in your UI to input the apiKey, optional parameters, body, function selection, and method selection.
+app.formCode = $(`
+    <h1>API Form</h1>
+  <form id="apiForm">
+    <label for="apiKey">API Key:</label>
+    <input type="text" id="apiKey" required><br><br>
+
+    <label for="optionalParams">Query Parameters:</label>
+    <input type="text" id="optionalParams"><br><br>
+
+    <label for="body">Body:</label>
+    <textarea id="body">{}</textarea><br><br>
+
+    <label for="function">Function:</label>
+    <select id="function">
+      <option value="sdk">SDK</option>
+      <option value="log">Log</option>
+      <option value="run">Run</option>
+      <option value="root">Root</option>
+      <option value="runOptions">Run Options</option>
+      <option value="fit">Fit</option>
+      <option value="routes">Routes</option>
+      <option value="runAtTime">Run At Time</option>
+    </select><br><br>
+
+    <label for="method">Method:</label>
+    <select id="method">
+      <option value="Post">POST</option>
+      <option value="Get">GET</option>
+    </select><br><br>
+
+    <button type="submit">Submit</button>
+  </form>
+    `);
+$('#content').html(''); // clear the content of the page
+$('#content').append(app.formCode);
+
+// Retrieve the input values
+var apiKey = document.getElementById('apiKey').value;
+var optionalParams = document.getElementById('optionalParams').value;
+var body = document.getElementById('body').value;
+var selectedFunction = document.getElementById('function').value;
+var selectedMethod = document.getElementById('method').value;
+
 $.fn.json_beautify = function () {
   // ref: https://stackoverflow.com/a/62060316/1871569
   try {
@@ -43,42 +87,6 @@ app.initializeApp = async () => {
 
   // a simple UI to input apiKey, enter any optional parameters + the body, and choose which function to call, and select the method (POST/GET). Make sure to validate the input. Handle websocket & HTTP endpoints.
 
-  // Create an HTML form in your UI to input the apiKey, optional parameters, body, function selection, and method selection.
-  app.formCode = $(`
-    <h1>API Form</h1>
-  <form id="apiForm">
-    <label for="apiKey">API Key:</label>
-    <input type="text" id="apiKey" required><br><br>
-
-    <label for="optionalParams">Query Parameters:</label>
-    <input type="text" id="optionalParams"><br><br>
-
-    <label for="body">Body:</label>
-    <textarea id="body">{}</textarea><br><br>
-
-    <label for="function">Function:</label>
-    <select id="function">
-      <option value="sdk">SDK</option>
-      <option value="log">Log</option>
-      <option value="run">Run</option>
-      <option value="root">Root</option>
-      <option value="runOptions">Run Options</option>
-      <option value="fit">Fit</option>
-      <option value="routes">Routes</option>
-      <option value="runAtTime">Run At Time</option>
-    </select><br><br>
-
-    <label for="method">Method:</label>
-    <select id="method">
-      <option value="Post">POST</option>
-      <option value="Get">GET</option>
-    </select><br><br>
-
-    <button type="submit">Submit</button>
-  </form>
-    `);
-  $('#content').html(''); // clear the content of the page
-  $('#content').append(app.formCode);
   $("#apiForm > select#function").val("run");
   $("#apiForm > select#function").on("change", function () {
     if (this.value == "sdk") {
@@ -99,11 +107,11 @@ app.initializeApp = async () => {
     event.preventDefault();
 
     // Retrieve the input values
-    var apiKey = document.getElementById('apiKey').value;
-    var optionalParams = document.getElementById('optionalParams').value;
-    var body = document.getElementById('body').value;
-    var selectedFunction = document.getElementById('function').value;
-    var selectedMethod = document.getElementById('method').value;
+    apiKey = document.getElementById('apiKey').value;
+    optionalParams = document.getElementById('optionalParams').value;
+    body = document.getElementById('body').value;
+    selectedFunction = document.getElementById('function').value;
+    selectedMethod = document.getElementById('method').value;
 
     // Validate the input values
     // ... (TODO)
@@ -114,24 +122,40 @@ app.initializeApp = async () => {
     // Handle WebSocket and HTTP endpoints based on the selected method and function
     // Call the appropriate function with the provided input values.
     try {
+
       console.log(selectedFunction, selectedMethod);
-      apigClient[selectedFunction + selectedMethod](optionalParams, body, {
+
+      var result = await apigClient[selectedFunction + selectedMethod](optionalParams, body, {
         headers: {
           Authorization: 'Bearer allow',
           Accept: '*/*'
         }
-      })
-        .then(async (response) => {
-          return await response.json();
-        })
-        .then((json_data) => {
-          $("#output-area").html(
-            `<pre><code>${json_data}</code></pre>`
-          );
-        })
-        .catch((err) => {
-          console.warn(`failed to access the specified endpoint: '${selectedFunction}${selectedMethod}'.\nError:`, err);
-        })
+      });
+
+      // set raw json data...
+      var data = result.data;
+      var string_data = JSON.stringify(data);
+      console.log(string_data);
+      $("#output-area").html(
+        `<code>${string_data}</code>`
+      );
+
+      // update chart...
+      const ctx = document.getElementById("chart");
+      if (selectedFunction == 'run') {
+        var arr = [];
+        data.t.forEach((value, index) => {
+          arr.push({
+            x: value,
+            y: data.G[index]
+          });
+        });
+        new Chart(ctx, {
+          type: 'scatter',
+          data: arr
+        });
+      }
+
     } catch (err) {
       console.warn(`failed to access the specified endpoint: '${selectedFunction}${selectedMethod}'.\nError:`, err);
     }
