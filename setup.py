7489062ae7f629f43a20e3444accec25d0a23a21
Robbie Capps <rocapp@gmail.com>
2023-09-25 13:05:36 -0700

    fixed minpack dependency

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='pfun-cma-model',
     version='0.1.0',
     description='PFun CMA Model',
-    packages=['pfun-cma-model', 'runtime', 'embed', 'reheater'],
+    packages=find_packages(),
 )
