640eeaabe78d710cba9f10f14a5cede2322529ff
Robbie Capps <rocapp@gmail.com>
2023-10-12 16:54:08 -0700

    simple ui

--- examples/dexcom-api.py
+++ examples/dexcom-api.py
@@ -1,61 +1,52 @@
-# %%
-# %pip install flask requests flask_oauthlib
-
-# %%
-import os
 import json
-creds = json.loads(open(os.path.expanduser('~/.credentials/dexcom_pfun-app_glucose.json')).read())
-os.environ['DEXCOM_CLIENT_ID'] = creds['client_id']
-os.environ['DEXCOM_CLIENT_SECRET'] = creds['client_secret']
-
-# %%
 import os
-from flask import Flask, redirect, url_for, session, request
-from flask_oauthlib.client import OAuth
+
+import requests
+from flask import Flask, redirect, request
 
 app = Flask(__name__)
-app.secret_key = 'random_secret_key'  # Change this to a random secret key
-oauth = OAuth(app)
-
-dexcom = oauth.remote_app(
-    'dexcom',
-    consumer_key=os.getenv('DEXCOM_CLIENT_ID'),
-    consumer_secret=os.getenv('DEXCOM_CLIENT_SECRET'),
-    base_url='https://api.dexcom.com/v2/',
-    request_token_url='https://api.dexcom.com/v2/oauth2/token',
-    access_token_method='POST',
-    access_token_url='https://api.dexcom.com/v2/oauth2/token',
-    authorize_url='https://api.dexcom.com/v2/oauth2/login'
-)
 
 
-@app.route('/')
-def index():
-    return 'Welcome to Dexcom Wrapper! <a href="/login">Login with Dexcom</a>'
-
-@app.route('/login')
-def login():
-    return dexcom.authorize(callback=url_for('authorized', _external=True))
-
-@app.route('/login/authorized')
-def authorized():
-    response = dexcom.authorized_response()
-    if response is None or response.get('access_token') is None:
-        return 'Could not authorize', 403
-
-    session['dexcom_token'] = (response['access_token'], '')
-    me = dexcom.get('users/self')
-    return 'Logged in as: {}'.format(me.data)
-
-@dexcom.tokengetter
-def get_dexcom_oauth_token():
-    return session.get('dexcom_token')
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+def get_creds():
+    creds = json.loads(
+        open(os.path.expanduser("~/.credentials/dexcom_pfun-app_glucose.json")).read()
+    )
+    os.environ["DEXCOM_CLIENT_ID"] = creds["client_id"]
+    os.environ["DEXCOM_CLIENT_SECRET"] = creds["client_secret"]
+    return creds["client_id"], creds["client_secret"]
 
 
-# %%
+client_id, client_secret = get_creds()
+redirect_uri = "http://127.0.0.1:5000/callback"
 
 
+@app.route("/")
+def home():
+    auth_url = f"https://api.dexcom.com/v2/oauth2/login?client_id={client_id}&redirect_uri={redirect_uri}&response_type=code&scope=offline_access"
+    return f'<a href="{auth_url}">Login with Dexcom</a>'
 
+
+@app.route("/callback")
+def callback():
+    auth_code = request.args.get("code")
+    token_url = "https://api.dexcom.com/v2/oauth2/token"
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    data = {
+        "client_id": client_id,
+        "client_secret": client_secret,
+        "code": auth_code,
+        "grant_type": "authorization_code",
+        "redirect_uri": redirect_uri,
+    }
+    response = requests.post(token_url, headers=headers, data=data)
+    if response.ok:
+        access_token = response.json().get("access_token")
+        return {"authorization": f"Bearer {access_token}", "access_token": access_token}
+    return {
+        "error": "Error: Unable to retrieve access token.",
+        "status_code": response.status_code,
+    }, response.status_code
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
