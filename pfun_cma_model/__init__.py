1965215cb47cefc2d2448f5da0172ec7743373e6
Robbie Capps <rocapp@gmail.com>
2025-09-11 00:21:34 -0700

    minor updates from docker-lxc

--- pfun_cma_model/__init__.py
+++ pfun_cma_model/__init__.py
@@ -3,7 +3,6 @@ from .misc.pathdefs import *
 from .engine.cma_model_params import *
 from .engine.cma import *
 from .engine.fit import *
-from .engine.cma_plot import *
 
 __all__ = [
     "PFunDataPaths",
@@ -11,7 +10,6 @@ __all__ = [
     "CMASleepWakeModel",
     "run_at_time_func",
     "cma_fit_model",
-    "cma_plot"
 ]
 
 # top-level convenience imports
