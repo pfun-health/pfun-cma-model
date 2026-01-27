5f23832027a9ab333932e1eafcf5b7bb389a675b
Robbie Capps <robbie@pfun.me>
2026-01-27 12:55:30 -0800

    (pfun-cma-model 0.4.161) bump to new version.

--- pfun_cma_model/app.py
+++ pfun_cma_model/app.py
@@ -1,8 +1,28 @@
 """pfun_cma_model/app.py : pfun-cma-model fastapi app definition."""
+from pfun_common.settings import get_settings
 import pfun_cma_model.api as api_core
 
 # Export the FastAPI app from api_core
 app = api_core.app
 
+# # #
+# setup the security layer
+# # #
+from guard import SecurityMiddleware
+from guard.models import SecurityConfig
+# Configure rate limiting
+config = SecurityConfig(
+    rate_limit=100,               # Max 100 requests
+    rate_limit_window=120,         # over X seconds
+    enable_rate_limiting=True,    # Enable rate limiting (true by default)
+    enable_redis=True,            # Use Redis for distributed setup (true by default)
+    redis_url=get_settings().redis_url
+)
+# Add middleware with rate limiting
+app.add_middleware(SecurityMiddleware, config=config)
+
+
+# # #
 # Export the socket-io session from api_core
+# # #
 socketio_session = api_core.socketio_session
--- pfun_cma_model/routes/llm.py
+++ pfun_cma_model/routes/llm.py
@@ -1,7 +1,7 @@
 """
 PFun CMA Model - LLM API Routes
 """
-
+import asyncio
 import json
 
 from fastapi import APIRouter, Response
@@ -18,10 +18,24 @@ This person is mostly healthy but occasionally eats a late dinner.
 
 @router.post("/generate-scenario")
 async def generate_scenario(prompt: str = DEFAULT_HEALTHY_PROMPT):
-    """Use VertexAI LLM endpoint to generate a realistic scenario (with hypothetical parameters)."""
-    response_data = await gen_scene(query=prompt)
+    """Use LLM endpoint to generate a realistic scenario (with hypothetical parameters)."""
+
+    async def attempt_scene_gen():
+        response_data = await gen_scene(query=prompt)
+        try:
+            content = json.dumps(response_data)
+        except json.JSONDecodeError as exc:
+            # try once more (after resting a moment)
+            await asyncio.sleep(1)
+            return await attempt_scene_gen()
+        else:
+            # if it works, return the content
+            return content
+    
+    content = await attempt_scene_gen()
+    
     return Response(
-        content=json.dumps(response_data),
+        content=content,
         status_code=200,
         headers={"Content-Type": "application/json"},
     )
