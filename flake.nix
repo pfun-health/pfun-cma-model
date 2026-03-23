bc222b9ab4e7c716f1f9ade3603e3aa5d5dab2ed
robbiec <rocapp@gmail.com>
2026-03-23 15:07:46 -0700

    messing with flake.nix (trying flake-parts, devenv, ...)

--- flake.nix
+++ flake.nix
@@ -3,88 +3,116 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devenv.url = "github:cachix/devenv";
+    nix2container = {
+      url = "github:nlewo/nix2container";
+      inputs = { nixpkgs.follows = "nixpkgs"; };
+    };
+    mk-shell-bin.url = "github:rrbutani/nix-mk-shell-bin";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+  outputs =
+    inputs@{
+      self,
+      flake-parts,
+      nixpkgs,
+      devenv,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+
+      imports = [
+        inputs.devenv.flakeModule
+      ];
+      systems = [ "x86_64-linux" ];
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          # As per pyproject.toml: >3.11,<3.13.
+          python = pkgs.python312;
+
+          # System-level dependencies for Python packages
+          # that might be built from source by `uv`.
+          # This ensures `uv sync` works smoothly.
+          python_build_deps = with pkgs; [
+            # for PyQt6
+            libGLU
+            libGL
+
+            # for matplotlib, scipy
+            freetype
+            tk
+            qhull
+
+            # for scipy, numpy
+            gfortran
+            openblas
+
+            # for pyarrow
+            arrow-cpp
+
+            # for numba (llvmlite)
+            llvm
+
+            # for pydantic (pydantic-core, which is a rust extension)
+            rustc
+            cargo
+
+            # for paramiko (cryptography)
+            openssl
+
+            # for various packages that might be installed
+            zlib
+
+            # duckdb
+            duckdb
+
+            # data visualization
+            #datasette  # currently isn't working due to dependency on pip module (should work once we have uv install the Python dependencies, but we want to be able to run `uv sync` without errors first)
+          ];
+        in
+        {
+          devenv.shells.default = {
+            # https://devenv.sh/reference/options/
+            name = "pfun-cma-model-dev";
+            packages =
+              with pkgs;
+              [
+                python # note that the version is defined above
+                uv
+                pkg-config # general-purpose build tool
+              ]
+              ++ python_build_deps;
+
+            enterShell = ''
+              echo "Entering the pfun-cma-model dev shell..."
+              echo "Setting LD_LIBRARY_PATH..."
+              export LD_LIBRARY_PATH="/run/opengl-driver/lib:/run/opengl-driver-32/lib"
+              echo "Installing dev dependencies..."
+              echo "...datasette plugins..."
+              datasette install datasette-parquet
+              datasette install datasette-plot
+              echo "...done installing dev dependencies."
+
+              echo "Welcome to the pfun-cma-model dev shell!"
+              echo ""
+              echo "This shell provides Python, uv."
+              echo "The project's Python dependencies are defined in pyproject.toml."
+              echo ""
+
+              . ./.venv/bin/activate
+              echo "Activated Python virtual environment from .venv/ directory."
+            '';
+          };
         };
-
-        # As per pyproject.toml: >3.11,<3.13.
-        python = pkgs.python312;
-
-        # System-level dependencies for Python packages
-        # that might be built from source by `uv`.
-        # This ensures `uv sync` works smoothly.
-        python_build_deps = with pkgs; [
-          # for PyQt6
-          libGLU
-          libGL
-          
-          # for matplotlib, scipy
-          freetype
-          tk
-          qhull
-
-          # for scipy, numpy
-          gfortran
-          openblas
-
-          # for pyarrow
-          arrow-cpp
-
-          # for numba (llvmlite)
-          llvm
-
-          # for pydantic (pydantic-core, which is a rust extension)
-          rustc
-          cargo
-
-          # for paramiko (cryptography)
-          openssl
-
-          # for various packages that might be installed
-          zlib
-
-          # duckdb
-          duckdb
-
-          # data visualization
-          #datasette  # currently isn't working due to dependency on pip module (should work once we have uv install the Python dependencies, but we want to be able to run `uv sync` without errors first)
-        ];
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          name = "pfun-cma-model-dev";
-
-          buildInputs = with pkgs; [
-            python  # note that the version is defined above
-            uv
-
-            # General purpose build tools
-            pkg-config
-          ] ++ python_build_deps;
-
-          shellHook = ''
-            echo "Installing dev dependencies..."
-            echo "...datasette plugins..."
-            datasette install datasette-parquet
-            datasette install datasette-plot
-            echo "...done installing dev dependencies."
-
-            echo "Welcome to the pfun-cma-model dev shell!"
-            echo ""
-            echo "This shell provides Python, uv."
-            echo "The project's Python dependencies are defined in pyproject.toml."
-            echo ""
-            
-            . ./.venv/bin/activate
-            echo "Activated Python virtual environment from .venv/ directory."
-          '';
-        };
-      }
-    );
+    };
 }
