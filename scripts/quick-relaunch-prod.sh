0ff0a89bea5db4429d24b38111d2f4436ba8b03b
Robbie Capps <robbie@pfun.me>
2026-04-16 07:58:29 -0700

    bugfixes and ux improvements for serve-prod

--- scripts/quick-relaunch-prod.sh
+++ scripts/quick-relaunch-prod.sh
@@ -1,1 +1,15 @@
-pkill -9 uvicorn; sleep 1s; ./scripts/serve-prod.sh full
+#!/usr/bin/env sh
+
+# quick-relaunch-prod.sh
+
+set -e
+
+echo "relaunching prod..."
+
+echo "killing uvicorn..."
+pkill -9 uvicorn
+echo "...killed using pkill (sleeping for 2s)."
+sleep 2s
+
+echo "serving prod..."
+./scripts/serve-prod.sh full
--- scripts/serve-prod.sh
+++ scripts/serve-prod.sh
@@ -3,10 +3,10 @@
 # scripts/serve-prod.sh : serve the current version of pfun-cma-model (no need for docker)
 
 # Load common functions
-export REPO_ROOT=$(dirname "$0")
-source "${REPO_ROOT}/_funcs.def.sh"
+export REPO_ROOT="${PWD}"
+source "${REPO_ROOT}/scripts/_funcs.def.sh"
 
-echo -e "Timestamp:$(timedatectl)\n"
+echo -e "RepoRoot:\t'${REPO_ROOT}'\n\nTimestamp:\n$(timedatectl)\n"
 
 # Start server full uv sync and serve pfun-cma-model, pfun-gradio
 if [ "$1" = 'cma' ]; then
@@ -16,7 +16,7 @@ else
 		full_uv_sync_gradio; serve_pfun_gradio
     else
 	if [ "$1" = 'full' ]; then
-	    nohup /usr/bin/env bash -c 'cd '"${REPO_ROOT}"'. ./.venv/bin/activate; scripts/serve-prod.sh cma' &
+	    nohup /usr/bin/env sh -c "echo 'serving...'; . ./.venv/bin/activate; ${REPO_ROOT}/scripts/serve-prod.sh cma" &
 	else
 	    echo 'no server selected (should be either gradio or cma)'
 	fi
