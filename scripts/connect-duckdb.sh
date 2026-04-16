786c9359d3ffffef422e74963014c35a5310df70
Robbie Capps <rocapp@gmail.com>
2026-04-15 18:02:56 -0700

    updated connect-duckdb script

--- scripts/connect-duckdb.sh
+++ scripts/connect-duckdb.sh
@@ -8,21 +8,19 @@ echo "Connecting to DuckDB and running test queries..."
 echo "  ===========================================  "
 
 
-echo -e "\nTesting cma_pgrid table:\n"
-echo "  ------------------------------------------------  "
-duckdb results/duckdb.db 'SELECT * FROM cma_pgrid LIMIT 10'
-duckdb results/duckdb.db 'SELECT COUNT(*) FROM cma_pgrid'
+# echo -e "\nTesting cma_pgrid table:\n"
+# echo "  ------------------------------------------------  "
+# duckdb results/duckdb-local.db 'SELECT * FROM cma_pgrid LIMIT 10'
+# duckdb results/duckdb-local.db 'SELECT COUNT(*) FROM cma_pgrid'
+# echo -e "\n===============================================\n"
 
-echo -e "\n===============================================\n"
-
-echo -e "\nTesting cma_scenes table:\n"
-echo "  ------------------------------------------------  "
-duckdb results/duckdb.db 'SELECT * FROM cma_scenes LIMIT 10'
-duckdb results/duckdb.db 'SELECT COUNT(*) FROM cma_scenes'
-
-echo -e "\n===============================================\n"
+# echo -e "\nTesting cma_scenes table:\n"
+# echo "  ------------------------------------------------  "
+# duckdb results/duckdb-local.db 'SELECT * FROM cma_scenes LIMIT 10'
+# duckdb results/duckdb-local.db 'SELECT COUNT(*) FROM cma_scenes'
+# echo -e "\n===============================================\n"
 
 echo -e "\nTesting cma_recs table:\n"
 echo "  ------------------------------------------------  "
-duckdb results/duckdb.db 'SELECT * FROM cma_recs LIMIT 10'
-duckdb results/duckdb.db 'SELECT COUNT(*) FROM cma_recs'
+duckdb results/duckdb-local.db 'SELECT * FROM cma_recs LIMIT 10'
+duckdb results/duckdb-local.db 'SELECT COUNT(*) FROM cma_recs'
