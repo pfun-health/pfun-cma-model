64ac6e6e70fce745c698b2510854a2ef9b9bf253
Robbie Capps <rocapp@gmail.com>
2026-04-25 03:38:02 -0700

    progress on pfun_cma_engine

--- pfun_cma_model/engine/pfun_cma_engine.h
+++ pfun_cma_model/engine/pfun_cma_engine.h
@@ -8,4 +8,8 @@ float exp(float x);
 
 float expit_pfun(float x);
 
+float calc_vdep_current(float v, float v1, float v2, float A=1.0, float B=1.0);
 
+float E_norm(float x);
+
+float normalize(x, float a = 0.0, float b = 1.0);
