e16d900cc2096bac4dbeddcbc5a4172aadd0d6d3
Robbie Capps <rocapp@gmail.com>
2026-05-16 23:49:16 -0700

    local changes

--- scripts/serve-datasette.sh
+++ scripts/serve-datasette.sh
@@ -5,5 +5,20 @@ set -e
 # scripts/serve-datasette.sh
 
 echo -e "running datasette in the background (using metadata.json config)"
-pkill -9 datasette; sleep 2s;
-nohup uv run datasette --metadata ./metadata.json &
+sleep 1s
+
+pkill -9 datasette
+
+rm logs/datasette.log; touch logs/datasette.log
+sleep 1s
+
+echo -e "starting datasette..."
+sleep 1s
+
+nohup \
+    uv run datasette \
+    --metadata ./metadata.json \
+    logs/datasette.log
+
+sleep 1s;
+cat logs/datasette.log
