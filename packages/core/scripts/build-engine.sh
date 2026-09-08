2afd69764f2deabf06285ce13d8bb54f3e52a862
robbie capps <rocapp@gmail.com>
2026-09-08 13:37:40 -0700

    a few fixes on this fairly cursed branch, on this fairly cursed repo...

--- packages/core/scripts/build-engine.sh
+++ packages/core/scripts/build-engine.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 set -e
-
-cd ../pfun-cma-engine-c
-mkdir -p build && cd build
-cmake ..
-make
+bash -c 'git clone https://github.com/pfun-health/pfun-cma-engine-c.git && cd pfun-cma-engine-c && make clean && make'
+sleep 1s
+echo -e "...done building pfun-cma-engine-c (packages/core)."
